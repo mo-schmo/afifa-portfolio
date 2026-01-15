e3f0a150f8818608d4afc28533d7a2355e33e5cc
mo-schmo <gh3173@wayne.edu>
2026-01-14 21:30:41 -0500

    Edit favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import "./layout.css";
-	import favicon from "$lib/assets/favicon.svg";
+	import favicon from "$lib/assets/favicon.ico";
 	import MarginPoses from "$lib/components/MarginPoses.svelte";
 
 	let { children } = $props();
